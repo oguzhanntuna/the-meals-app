08a77b5a7b121d40d19d3788e6f8262e24808217
oguzhana.tuna <oguzhana.tuna@gmail.com>
2021-05-09 22:40:46 +0300

    grid styling and some refactoring

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import { enableScreens } from 'react-native-screens';
 
 import MealsNavigator from './navigation/MealsNavigator';
@@ -8,7 +8,7 @@ import MealsNavigator from './navigation/MealsNavigator';
 enableScreens();
 
 const fetchFonts = () => {
-  Font.loadAsync({
+  return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
@@ -21,7 +21,7 @@ export default function App() {
     return (
       <AppLoading
         startAsync={fetchFonts}
-        onFinish={setFontLoaded(true)}
+        onFinish={() => setFontLoaded(true)}
         onError={(error) => console.log(error)}
       />
     );
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Text, View, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity style={styles.gridItem} onPress={() => {
-                props.navigation.navigate({
-                    routeName: 'CategoryMeals', params: {
-                        categoryId: itemData.item.id
-                    }
-                }); 
-            }}>
-                <View>
-                    <Text>
-                        {itemData.item.title}
-                    </Text>
-                </View>
-            </TouchableOpacity>
+            <CategoryGridTile
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onSelect={() => {
+                    props.navigation.navigate({
+                        routeName: 'CategoryMeals', params: {
+                            categoryId: itemData.item.id
+                        }
+                    });
+                }}
+            />
         );
-    }
+    };
 
     return (
         <FlatList
@@ -41,11 +40,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150
     }
 });
 
