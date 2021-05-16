df0f13bce8bd7f17c9373c0f4515ff06bd95f36d
oguzhana.tuna <oguzhana.tuna@gmail.com>
2021-05-16 19:53:07 +0300

    dispatching filter actions

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Text, View, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useDispatch } from 'react-redux';
 
 import Colors from '../const/Colors';
 import HeaderButton from '../components/HeaderButton';
+import { setFilters } from '../store/actions/meals';
 
 const FilterSwitch = props => {
     return (
@@ -25,6 +27,8 @@ const FiltersScreen = props => {
     const [isVegan, setIsVegan] = useState(false);
     const [isVegetarian, setIsVegetarian] = useState(false);
 
+    const dispatch = useDispatch();
+
     const saveFilters = useCallback(() => {
         const appliedFilters = {
             glutenFree: isGlutenFree,
@@ -33,8 +37,8 @@ const FiltersScreen = props => {
             vegetarian: isVegetarian
         }
 
-        console.log(appliedFilters);
-    }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+        dispatch(setFilters(appliedFilters));
+    }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian, dispatch]);
 
     useEffect(() => {
         props.navigation.setParams({
