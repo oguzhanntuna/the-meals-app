874af445def5944fe337f558f7aca80ddabb5c4b
oguzhana.tuna <oguzhana.tuna@gmail.com>
2021-05-16 16:47:43 +0300

    redux and store setup, selecting state slices

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
@@ -31,8 +31,10 @@ const RenderSteps = props => {
 };
 
 const MealDetailScreen = props => {
+    const avaibleMeals = useSelector(state => state.meals.meals);
     const mealId = props.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+    const selectedMeal = avaibleMeals.find(meal => meal.id === mealId);
 
     return (
         <ScrollView>
@@ -51,11 +53,10 @@ const MealDetailScreen = props => {
 };
 
 MealDetailScreen.navigationOptions = navigationData => {
-    const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const mealTitle = navigationData.navigation.getParam('mealTitle');
 
     return {
-        headerTitle: selectedMeal.title,
+        headerTitle: mealTitle,
         headerRight: (
             <HeaderButtons HeaderButtonComponent={HeaderButton} >
                 <Item
